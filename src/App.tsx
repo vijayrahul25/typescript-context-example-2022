9c52c0577ab5519242975b368445bafc16829f1a
vijayrahul <vijayrahul25@gmail.com>
2022-02-24 00:06:22 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <div className="row">
+      <div className="col">
+            Testing first react app
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
